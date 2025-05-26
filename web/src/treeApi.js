c8c149330eda36031ccc7f7e8631a13ee330a9fd
Alex Alekhin <work.alekhin@gmail.com>
2025-05-26 18:07:03 +0300

    gh-pages fix

--- web/src/treeApi.js
+++ web/src/treeApi.js
@@ -2,7 +2,7 @@ export async function createTree() {
   if (!window.TreeModule) {
     await new Promise((resolve, reject) => {
       const script = document.createElement('script');
-      script.src = '/tree.js';
+      script.src = './tree.js';
       script.onload = resolve;
       script.onerror = reject;
       document.body.appendChild(script);
